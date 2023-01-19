80866540d2f5c3c00a7bb262b41c156f334cd906
Claude Allen <claudeallen@Claudes-MacBook-Pro.local>
2023-01-19 09:04:00 -0600

    trying to upadte master properly

--- src/pages/ItemShow.js
+++ src/pages/ItemShow.js
@@ -1,55 +1,48 @@
-import { useState, useEffect } from 'react'
-import { useParams, useNavigate, Link } from 'react-router-dom'
-import Distance from '../components/Distance';
-
-
+import { useState, useEffect } from "react";
+import { useParams, useNavigate, Link } from "react-router-dom";
+import Distance from "../components/Distance";
 
 const ItemShow = (props) => {
-const { id } = useParams();
-const navigate = useNavigate();
-const item = props.items ? props.items.find((item)=>item._id === id):null;
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const item = props.items ? props.items.find((item) => item._id === id) : null;
 
+  const [editForm, setEditForm] = useState(item);
 
-const [editForm, setEditForm] = useState(item)
-
-useEffect(() => {
-  if(item) {
-    setEditForm(item)
-  }
-}, [item])
-
+  useEffect(() => {
+    if (item) {
+      setEditForm(item);
+    }
+  }, [item]);
 
   return (
-    <div className='itemShow'>
-    <div className='itemImg'>
-        <img src={item.image}/>
-    </div>
-
-    <div className='itemData'>
-            <li>{item.name}</li>
-            <li>{item.condition}</li>
-            <li>{item.description}</li>
-            <li>LINK TO OWNER PROFILE</li>
+    <div className="itemShow">
+      <div className="itemImg">
+        <img src={item.image} />
       </div>
-      <button onClick={handleDelete}>Delete This Listing</button> 
+
+      <div className="itemData">
+        <li>{item.name}</li>
+        <li>{item.condition}</li>
+        <li>{item.description}</li>
+        <li>LINK TO OWNER PROFILE</li>
+      </div>
+      <button onClick={handleDelete}>Delete This Listing</button>
       <Link to={`/items/update/${item._id}`}>Update Listing</Link>
     </div>
-  )
-}
+  );
+};
+
 
 const loading = () => {
-  return <h2>Loading...</h2>
-}
+  return <h2>Loading...</h2>;
+};
 
 const handleDelete = () => {
-  props.deleteItem(id)
-  navigate('/items')
-}
+    const navigate = useNavigate();
+  props.deleteItem(id);
+  navigate("/items");
+  return <section>{props.items ? loaded() : loading()}</section>;
+};
 
-return (
-  <section>
-    { props.items ? loaded() : loading() }
-  </section>
-)
-}
-export default ItemShow
+export default ItemShow;
